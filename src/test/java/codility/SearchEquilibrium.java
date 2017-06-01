7406984405a35e3b8fa94b86ec7c6d132a3c6ab2
JaeYeopHan <ljyhanll@gmail.com>
2017-06-01 22:37:08 +0900

    Add codility exercise - 1

---
+++ src/test/java/codility/SearchEquilibrium.java
@@ -0,0 +1,47 @@
+package codility;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SearchEquilibrium {
+
+    @Test
+    public void test() {
+        int[] arr = new int[9];
+        arr[0] = -4;
+        arr[1] = 8;
+        arr[2] = 16;
+        arr[3] = -22;
+        arr[4] = 0;
+        arr[5] = -4;
+        arr[6] = 8;
+        arr[7] = 16;
+        arr[8] = -22;
+        assertThat(true, is(solution(arr) == 0 || solution(arr) == 4));
+    }
+
+    public int solution(int[] arr) {
+        if (arr == null) return -1;
+
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+
+        if (total - arr[0] == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        for (int i = 1; i < arr.length; i++) {
+            left += arr[i - 1];
+            if ((total - arr[i])/2 == left) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
