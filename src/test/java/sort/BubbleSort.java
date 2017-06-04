5f81fa2d2faa079f1d551beeb426292f8c1a1c2b
JaeYeopHan <ljyhanll@gmail.com>
2017-06-04 20:04:51 +0900

    Add bubble sort code

--- src/test/java/sort/BubbleSort.java
+++ src/test/java/sort/BubbleSort.java
@@ -1,6 +1,7 @@
 package sort;
 
 import org.junit.Test;
+import utils.Utils;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -14,13 +15,7 @@ public class BubbleSort {
 
     @Test
     public void test() {
-        int[] arr = new int[5];
-        arr[0] = 2;
-        arr[1] = 1;
-        arr[2] = 4;
-        arr[3] = 0;
-        arr[4] = 3;
-
+        int[] arr = {2,1,4,0,3};
         int[] sortedArr = new int[arr.length];
         for (int i = 0; i < sortedArr.length; i++) {
             sortedArr[i] = i;
@@ -32,9 +27,7 @@ public class BubbleSort {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                    Utils.swapValue(arr, i, j);
                 }
             }
         }
