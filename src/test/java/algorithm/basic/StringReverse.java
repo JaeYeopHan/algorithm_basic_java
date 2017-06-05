c4b0bd71648e118011b85a9baa5708891c9e4dbb
JaeYeopHan <ljyhanll@gmail.com>
2017-06-05 21:56:11 +0900

    Add utils method

---
+++ src/test/java/algorithm/basic/StringReverse.java
@@ -0,0 +1,7 @@
+package algorithm.basic;
+
+/**
+ * Created by Jbee on 2017. 6. 5..
+ */
+public class StringReverse {
+}
--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -25,4 +25,11 @@ public class Utils {
         arr[b] = temp;
         return arr;
     }
+
+    public static char[] swapValue(char[] arr, int a, int b) {
+        char temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+        return arr;
+    }
 }
