686cdc23d3a756fcc5635d34e61f0d97c8c9c7de
JaeYeopHan <ljyhanll@gmail.com>
2017-06-06 19:46:22 +0900

    Add fibonacci code

--- src/test/java/algorithm/recursion/Fibonacci.java
+++ src/test/java/algorithm/recursion/Fibonacci.java
@@ -11,17 +11,56 @@ public class Fibonacci {
         TASK
         Fibonacci 를 계산하는 함수를 작성한다.
      */
+
     @Test
     public void test() {
-        assertThat(calcFibo(5), is(5));
-        assertThat(calcFibo(6), is(8));
-        assertThat(calcFibo(7), is(13));
+
+        assertThat(calcFiboByFor(5), is(5));
+        assertThat(calcFiboByFor(6), is(8));
+        assertThat(calcFiboByFor(7), is(13));
+
+        assertThat(calcFiboByRec(5), is(5));
+        assertThat(calcFiboByRec(6), is(8));
+        assertThat(calcFiboByRec(7), is(13));
+
+        assertThat(fiboByDp(5), is(5));
+        assertThat(fiboByDp(6), is(8));
+        assertThat(fiboByDp(7), is(13));
     }
 
-    public int calcFibo(int num) {
-        if (num < 2) {
-            return num;
+    // Iteration version
+    public int calcFiboByFor(int num) {
+        if (num < 0) return -1;
+
+        int curValue = 0;
+        int prevValue = 1;
+        int prevPrevValue = 0;
+
+        for (int i = 2; i <= num; i++) {
+            curValue = prevValue +prevPrevValue;
+            prevPrevValue = prevValue;
+            prevValue = curValue;
         }
-        return calcFibo(num - 1) + calcFibo(num - 2);
+        return curValue;
+    }
+
+    // Recursion version
+    public int calcFiboByRec(int num) {
+        if (num < 0) return -1;
+        if (num < 2) return num;
+        return calcFiboByRec(num - 1) + calcFiboByRec(num - 2);
+    }
+
+    // Dynamic Programming version
+    public int fiboByDp(int num) {
+        return calcFibo(num, new int[num + 1]);
+    }
+
+    private int calcFibo(int num, int[] cache) {
+        if (num < 2) return num;
+        if (cache[num] != 0) return cache[num];//in java, int[] is initialized by 0.
+
+        cache[num] = calcFibo(num - 1, cache) + calcFibo(num - 2, cache);
+        return cache[num];
     }
 }
